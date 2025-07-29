3a6dde68d9aefe0b43acd6189d08893f156e05d6
ANJILA <anjilas241@gmail.com>
2025-07-29 12:48:00 +0545

    feat:added google sheet contact form

--- script.js
+++ script.js
@@ -11,3 +11,23 @@
         event.currentTarget.classList.add("active-link");
         document.getElementById(tabname).classList.add("active-tab");
     }
+
+    
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyJXBDj48mod8-ZKI-x1v5tJvCxgznTvu3CSTOhCF09aTamKhZtWKXKrE5Sg6EF8DRmAg/exec'
+  const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message sent successfully!"
+        setTimeout(function()
+        {
+          msg.innerHTML="";
+        },5000);
+        form.reset()
+        
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
